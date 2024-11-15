7ac780be7200bbdfb941832ebdea76afb5284b64
Francis <francisco@talestudios.com>
2024-11-15 15:51:10 +0100

    Rename and use of size_t for size.

--- darray.c
+++ darray.c
@@ -1,66 +1,71 @@
 #include "darray.h"
-
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct Darray
 {
 	int capacity;
-	int last_pos;
-	int* elements;
+	size_t length;
+	size_t element_size;
+	unsigned char* elements;
 } Darray;
 
-
 //TODO: Using asserts
-Darray* Darray_create(int initial_capacity)
+Darray* Darray_create(int initial_capacity, size_t bytes_per_element)
 {
 	Darray* my_darray = malloc(sizeof(Darray));
 	my_darray -> capacity = initial_capacity;
-	my_darray -> last_pos = 0;
-	my_darray -> elements = malloc(sizeof(int) * initial_capacity);
+	my_darray -> length = 0;
+	my_darray -> element_size = bytes_per_element;
+	my_darray -> elements = malloc(initial_capacity * bytes_per_element);
 	return my_darray;
 }
 
-void Darray_add(Darray* array, int elem)
+void Darray_append(Darray* array, void* elem)
 {
 	//TODO: Handle enlarging the array
-	array->elements[array->last_pos++] = elem;
+	memcpy(array->elements + array->length * array->element_size, elem, array->element_size);
+	++array->length;
 }
 
-int Darray_get(Darray* array, int pos)
+void Darray_add(Darray* array, int pos, void* elem)
 {
-	return array->elements[pos];
+	//TODO: Handle enlarging the array
+	memcpy(array->elements + pos * array->element_size, elem, array->element_size);
+	++array->length;
+}
+
+
+void* Darray_get(Darray* array, int pos)
+{
+	return array->elements + pos * array->element_size;
 }
 
 void Darray_remove(Darray* array, int pos)
 {
-	for (int i = pos;
+	return;
+	/*for (int i = pos;
 		i < array -> last_pos;
 		++i)
 	{
 		array->elements[i] = array->elements[i+1];
 	}
 
-	--array->last_pos;
+	--array->last_pos;*/
 }
 
-int Darray_size(Darray* array)
+size_t Darray_length(Darray* array)
 {
-	return array->last_pos;
+	return array->length;
 }
 
 void Darray_debug(Darray *array)
 {
-	int size = Darray_size(array);
-	printf("Size: %d\n", size);
-	for (int i = 0; i < size; ++i)
+	int length = Darray_length(array);
+	printf("Size: %d\n", length);
+	/*for (int i = 0; i < size; ++i)
 	{
 		printf("Pos[%d] = %d\n", i, Darray_get(array, i));
-	}
-}
-
-Darray Darray_new()
-{
-	Darray darray;
-	return darray;
+	}*/
 }
--- darray.h
+++ darray.h
@@ -1,13 +1,16 @@
 #ifndef DARRAY_H
 #define DARRAY_H
 
+#include <stddef.h>
+
 typedef struct Darray Darray;
 
-Darray* Darray_create(int initial_capacity);
-void Darray_add(Darray* array, int elem);
-int Darray_get(Darray* array, int pos);
+Darray* Darray_create(int initial_capacity, size_t bytes_per_element);
+void Darray_append(Darray* array, void* elem);
+void Darray_add(Darray* array, int pos, void* elem);
+void* Darray_get(Darray* array, int pos);
 void Darray_remove(Darray* array, int pos);
-int Darray_size(Darray* array);
+size_t Darray_length(Darray* array);
 void Darray_debug(Darray* array);
 
 #endif
